db83627617dfca41be191d490e6d50ce20874ad5
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-04-26 15:55:19 +0200

    implement dynamic

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -15,10 +21,53 @@ export class DynamicPageComponent {
     ]),
   });
 
+  public newFavorite: FormControl = new FormControl('', Validators.required);
+
   constructor(private formBuilder: FormBuilder) {}
 
   get favorites() {
     return this.myForm.get('favorites') as FormArray;
+  }
+  isFieldValid(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+  isFieldValidInArray(formArray: FormArray, index: number): boolean | null {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) {
+      return null;
+    }
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      console.log(key);
+      switch (key) {
+        case 'required':
+          return 'this field is required';
+        case 'minlength':
+          return `minimum of ${errors['minlength'].requiredLength} characters.`;
+      }
+    }
+
+    return null;
+  }
+
+  onAdd(): void {
+    if (this.newFavorite.invalid) {
+      return;
+    }
+
+    this.favorites.push(
+      this.formBuilder.control(this.newFavorite.value, Validators.required)
+    );
+
+    this.newFavorite.reset();
   }
 
   onSubmit(): void {
@@ -27,6 +76,14 @@ export class DynamicPageComponent {
       return;
     }
     console.log(this.myForm.value);
+    (this.myForm.controls['favorites'] as FormArray) = this.formBuilder.array(
+      []
+    );
     this.myForm.reset();
+  }
+
+  onDelete(index: number): void {
+    // * since you already have a getter that returns favorites, then why not just use it. AND since in js everything passes thorugh reference, then a modification directly at favorites will delete directly the item in the array
+    this.favorites.removeAt(index);
   }
 }
