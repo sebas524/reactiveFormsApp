ea24f05f087c569a299af75dd902987f6a0c56f3
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-04-26 17:34:21 +0200

    implement switches

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required], []],
+    email: ['', [Validators.required], []],
+    username: ['', [Validators.required], []],
+    password: ['', [Validators.required, Validators.minLength(4)], []],
+    confirmPassword: ['', [Validators.required], []],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  isFieldValid(field: string) {
+    //TODO: get validation from special server
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
   templateUrl: './switches-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+  public myForm: FormGroup = this.formBuilder.group({
+    gender: ['M', [Validators.required], []],
+    wantNotifications: [true, [Validators.required], []],
+    termsAndConditions: [false, [Validators.requiredTrue], []],
+  });
 
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  };
+
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+  isFieldValid(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+
+      return;
+    }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+    this.person = newPerson;
+
+    console.log(this.myForm.value);
+  }
 }
